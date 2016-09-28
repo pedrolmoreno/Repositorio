112fbdb9de999b5abf83e1bf1a2008f661626303
Edu <edux328@gmail.com>
2016-09-28 17:49:21 -0300

    added TestController

---
+++ WebAppAspNet1/WebAppAspNet1/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppAspNet1.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            string message = "Ok";
+
+            return Json(message);
+        }
+
+    }
+}
